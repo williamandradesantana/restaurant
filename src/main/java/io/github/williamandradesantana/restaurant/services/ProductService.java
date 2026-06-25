0cb79b6e250433a7ea6972a1a0a8fb959e80406c
William Santana <williamandrade1058@gmail.com>
2026-06-25 16:58:11 -0300

    feat(product): creating service

---
+++ src/main/java/io/github/williamandradesantana/restaurant/services/ProductService.java
@@ -0,0 +1,63 @@
+package io.github.williamandradesantana.restaurant.services;
+
+import io.github.williamandradesantana.restaurant.domain.entity.ProductCategoryEntity;
+import io.github.williamandradesantana.restaurant.domain.entity.ProductEntity;
+import io.github.williamandradesantana.restaurant.dtos.ProductRequest;
+import io.github.williamandradesantana.restaurant.dtos.ProductResponse;
+import io.github.williamandradesantana.restaurant.repositories.ProductCategoryRepository;
+import io.github.williamandradesantana.restaurant.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final ProductCategoryRepository productCategoryRepository;
+
+    public ProductService(ProductRepository productRepository, ProductCategoryRepository productCategoryRepository) {
+        this.productRepository = productRepository;
+        this.productCategoryRepository = productCategoryRepository;
+    }
+
+    public Page<ProductResponse> listProducts(Pageable pageable) {
+        return productRepository.findAll(pageable).map(ProductResponse::fromEntity);
+    }
+
+    public ProductResponse getOneProduct(Long id) {
+        ProductEntity product = searchProductById(id);
+        return ProductResponse.fromEntity(product);
+    }
+
+    public ProductResponse createProduct(ProductRequest request) {
+        ProductCategoryEntity category = searchCategoryById(request.categoryId());
+        ProductEntity product = request.toEntity(category);
+
+        ProductEntity productSaved = productRepository.save(product);
+
+        return ProductResponse.fromEntity(productSaved);
+    }
+
+    public ProductResponse updateOneProduct(Long id, ProductRequest request) {
+        ProductEntity product = searchProductById(id);
+        ProductCategoryEntity category = searchCategoryById(request.categoryId());
+
+        request.insert(product, category);
+
+        ProductEntity productUpdated = productRepository.save(product);
+        return ProductResponse.fromEntity(productUpdated);
+    }
+
+    public void delete(Long id) {
+        ProductEntity product = searchProductById(id);
+        productRepository.delete(product);
+    }
+
+    private ProductCategoryEntity searchCategoryById(Long id) {
+        return productCategoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category not found!"));
+    }
+    private ProductEntity searchProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found!"));
+    }
+}
