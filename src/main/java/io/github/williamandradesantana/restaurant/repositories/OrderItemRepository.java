17ba8136ff63dff01a16471005c495f743b6c846
William Santana <williamandrade1058@gmail.com>
2026-06-26 12:16:20 -0300

    feat(order-item): list items by id

--- src/main/java/io/github/williamandradesantana/restaurant/repositories/OrderItemRepository.java
+++ src/main/java/io/github/williamandradesantana/restaurant/repositories/OrderItemRepository.java
@@ -3,5 +3,8 @@ package io.github.williamandradesantana.restaurant.repositories;
 import io.github.williamandradesantana.restaurant.domain.entity.OrderItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItemEntity, Long> {
+    List<OrderItemEntity> findByOrder_Id(Long orderId);
 }
